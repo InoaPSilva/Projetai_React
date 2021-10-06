a08825b2972b537d184b0f15c15ef69295e0d5c0
Augusto Wesley <62189499+Augustowtg@users.noreply.github.com>
2021-10-05 22:29:16 -0300

    bugfix/Erro na importação dos components

---
+++ src/styled/ThemesStyled.js
@@ -0,0 +1,10 @@
+const theme = {
+    colors: {
+        cSky: '#CAEBF2',
+        cCarbon: '#A9A9A9',
+        cWatermelon: '#FF3B3F',
+        cNeutral: '#EFEFEF',
+        cWhite: '#FFFFFF'
+    }
+}
+export default theme
