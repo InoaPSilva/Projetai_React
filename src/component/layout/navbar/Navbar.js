044651d57b6f3d9da37833b10abc00da31bc7f0d
Augusto Wesley <62189499+Augustowtg@users.noreply.github.com>
2021-10-11 13:26:29 -0300

    aaa

---
+++ src/component/layout/navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+// Import components from react-bootstrap
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import { StyledHeader, NavbarStyled, NavBrandStlyled } from "../../../styled/components/HeaderStyled";
+import LinkStyled from "../../../styled/layout/LinkStyled";
+
+// Import  the menu
+import { NavList } from "./navList";
+
+function NavbarComponent() {
+    return (
+        <StyledHeader>
+            <NavbarStyled collapseOnSelect expand="lg">
+                <Container>
+                    <NavBrandStlyled href="/">Projetai</NavBrandStlyled>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="m-auto">
+                            {NavList.map((item, index) => {
+                                return (
+                                    <LinkStyled 
+                                        key={index}
+                                        className={item.cName}
+                                        href={item.url}>{item.title}</LinkStyled>
+                                )
+                            })}
+                        </Nav>
+                        <Nav>
+                            {/* Mudar para um icone usando React icons :D */}
+                            <NavDropdown title="Mais" className="">
+                                <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </NavbarStyled>
+        </StyledHeader>
+    )
+}
+export default NavbarComponent;
