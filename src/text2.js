4881652cd8ce5a197f6370f1a6acd6befb8f66e8
Augusto Wesley <62189499+Augustowtg@users.noreply.github.com>
2021-10-15 10:31:02 -0300

    Att

--- src/text2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Text2() {
-    return(
-        <button>teste</button>
-    )
-}  
--- src/usersPage/userProfile.js
+++ src/usersPage/userProfile.js
@@ -1,17 +1,21 @@
+import React, { useEffect } from "react";
 import Http from "../Api";
 
 export default function UserProfile() {
 
-    (async () => {
-        Http.get(`user/display/${localStorage.token}`)
-            .then((response) => {
-                const { data: { messege } } = response;
-                console.log(messege);
-            }).catch((err) => {
-                console.log(err)
-            })
-
-    })()
+    useEffect(() => {
+        (async () => {
+            Http.get(`user/display/${localStorage.token}`)
+                .then((response) => {
+                    const { data: { messege } } = response;
+                    console.log(messege);
+                }).catch((err) => {
+                    console.log(err)
+                })
+    
+        })()
+    }, [])
+    
 
     return (
 
